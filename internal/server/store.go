d2ec4cab2042a6167f084edef21f6222ded57fa2
an <habinhan1999@gmail.com>
2022-08-31 16:57:30 +0700

    implement store

---
+++ internal/server/store.go
@@ -0,0 +1,100 @@
+package server
+
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+	"sync"
+)
+
+var (
+	enc = binary.BigEndian
+)
+
+const (
+	lenWidth = 8
+)
+
+type store struct {
+	*os.File
+	mu   sync.Mutex
+	buf  *bufio.Writer
+	size uint64
+}
+
+func NewStore(f *os.File) (*store, error) {
+	file, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+
+	return &store{
+		File: f,
+		buf:  bufio.NewWriter(f),
+		size: uint64(file.Size()),
+	}, nil
+}
+
+func (s *store) Append(data []byte) (uint64, uint64, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	pos := s.size
+	if err := binary.Write(s.buf, enc, uint64(len(data))); err != nil {
+		return 0, 0, err
+	}
+
+	nn, err := s.buf.Write(data)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	nn += lenWidth
+	s.size += uint64(nn)
+	return uint64(nn), pos, nil
+}
+
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+
+	size := make([]byte, lenWidth)
+	_, err := s.File.ReadAt(size, int64(pos))
+	if err != nil {
+		return nil, err
+	}
+
+	data := make([]byte, enc.Uint64(size))
+	_, err = s.File.ReadAt(data, int64(pos+lenWidth))
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (s *store) ReadAt(p []byte, pos int64) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+
+	return s.File.ReadAt(p, pos)
+}
+
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if err := s.buf.Flush(); err != nil {
+		return err
+	}
+
+	return s.File.Close()
+}
---
+++ internal/server/store_test.go
@@ -0,0 +1,115 @@
+package server
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func Test_store_Append(t *testing.T) {
+	file, _ := os.CreateTemp("", "store_append_test")
+	s, _ := NewStore(file)
+
+	type args struct {
+		data []byte
+	}
+	tests := []struct {
+		name             string
+		args             args
+		wantRecordLength uint64
+		wantPos          uint64
+	}{
+		{
+			name: "test write hello",
+			args: args{
+				data: []byte("hello"),
+			},
+			wantRecordLength: 13,
+			wantPos:          0,
+		},
+		{
+			name: "test write word",
+			args: args{
+				data: []byte(" world"),
+			},
+			wantRecordLength: 14,
+			wantPos:          13,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			recordLength, pos, err := s.Append(tt.args.data)
+			if err != nil {
+				t.Errorf("Append() error = %v", err)
+				return
+			}
+			if recordLength != tt.wantRecordLength {
+				t.Errorf("Append() recordLength = %v, wantRecordLength %v", recordLength, tt.wantRecordLength)
+			}
+			if pos != tt.wantPos {
+				t.Errorf("Append() pos = %v, wantPos %v", pos, tt.wantPos)
+			}
+		})
+	}
+}
+
+func Test_store_Append_Read(t *testing.T) {
+	content := "test"
+	file, _ := os.CreateTemp("", "store_append_test")
+	defer file.Close()
+
+	s, _ := NewStore(file)
+
+	u, u2, err := s.Append([]byte(content))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(u, u2)
+
+	data, err := s.Read(u2)
+	if err != nil {
+		panic(err)
+	}
+
+	if string(data) != content {
+		t.Errorf("want %v, got %v", content, string(data))
+	}
+
+	fmt.Printf("got: %q, want: %v\n", data, content)
+}
+
+func Test_store_ReadAt(t *testing.T) {
+	content := "abcd"
+	file, _ := os.CreateTemp("", "store_append_test")
+	defer file.Close()
+
+	s, _ := NewStore(file)
+
+	u, u2, err := s.Append([]byte(content))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("u", u, "u2", u2)
+
+	_, u1, _ := s.Append([]byte(content))
+	fmt.Println("u1", u1)
+	b := make([]byte, u)
+	test, err := s.ReadAt(b, int64(u1-1))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("readAt %q\n", test)
+
+	data, err := s.Read(u2)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(data))
+
+	if string(data) != content {
+		t.Errorf("want %v, got %v", content, string(data))
+	}
+}
