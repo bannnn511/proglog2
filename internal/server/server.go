a511fc2af6d93b96ead6d0c4505551a04f0d84c0
an <habinhan1999@gmail.com>
2022-09-16 17:25:15 +0700

    implement grpc service

---
+++ internal/server/server.go
@@ -0,0 +1,84 @@
+package server
+
+import (
+	"context"
+	api "proglog/api/v1"
+)
+
+type Config struct {
+	CommitLog CommitLog
+}
+
+type CommitLog interface {
+	Append(record *api.Record) (uint64, error)
+	Read(offset uint64) (*api.Record, error)
+}
+
+type grpcServer struct {
+	api.UnimplementedLogServer
+	*Config
+}
+
+func newgrpcServer(config *Config) (*grpcServer, error) {
+	grpcServer := &grpcServer{
+		Config: config,
+	}
+
+	return grpcServer, nil
+}
+
+func (s *grpcServer) Produce(_ context.Context, req *api.ProduceRequest) (*api.ProduceResponse, error) {
+	offset, err := s.CommitLog.Append(req.Record)
+	if err != nil {
+		return nil, err
+	}
+
+	return &api.ProduceResponse{Offset: offset}, nil
+}
+
+func (s *grpcServer) Consume(_ context.Context, req *api.ConsumeRequest) (*api.ConsumeResponse, error) {
+	record, err := s.CommitLog.Read(req.Offset)
+	if err != nil {
+		return nil, err
+	}
+
+	return &api.ConsumeResponse{Record: record}, nil
+}
+
+func (s *grpcServer) ProduceStream(stream api.Log_ProduceStreamServer) error {
+	for {
+		recv, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+
+		offset, err := s.CommitLog.Append(recv.Record)
+		if err != nil {
+			return err
+		}
+
+		err = stream.Send(&api.ProduceResponse{Offset: offset})
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func (s *grpcServer) ConsumeStream(req *api.ConsumeRequest, stream api.Log_ConsumeStreamServer) error {
+	for {
+		select {
+		case <-stream.Context().Done():
+			return nil
+		default:
+			res, err := s.Consume(stream.Context(), req)
+			if err != nil {
+				return err
+			}
+
+			if err = stream.Send(res); err != nil {
+				return err
+			}
+			req.Offset++
+		}
+	}
+}
